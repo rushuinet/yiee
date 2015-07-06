9e10a69ff117d93f6d8048a9d4752433ee91ef2b
rushui <rushui@5291143e-60c1-428a-9f47-b12c1fb71de0>
2015-07-06 07:26:47 +0000

    优化视图基类

--- system/core/View.php
+++ system/core/View.php
@@ -7,8 +7,8 @@
 class View{
 	//模板输出变量
 	 protected $_view_data = array();
-	//布局
-	 protected $_layout = '';
+	//布局文件
+	 protected $_layout_path = '';
 
 	/**
 	 * 模板变量赋值
@@ -41,14 +41,15 @@ class View{
 	 * @param mixed $value
 	 */
 	public function layout($name){
-		$this->_layout = $name;
+		$file_suffix = $this->get_suffix($name);
+		$this->_layout_path = $name.$file_suffix;
 	}
 
 	/**
 	 * 输出视图
 	 * param	$__name__	视图文件
 	 * param	$__data__	分配的数据
-	 * param	$way	输出方式(true返回字符串,false直接引入文件)
+	 * param	$__way__	输出方式(true返回字符串,false直接引入文件)
 	 * @E-mail	rushui@qq.com
 	 * @author	Rushui
 	 */
@@ -58,13 +59,15 @@ class View{
 		}
 		//路径组装
 		$__view_path__ = Yiee::conf('view_path');
-		$__view_suffix__ = Yiee::conf('view_suffix') ? Yiee::conf('view_suffix') : '.php';
 		if( empty($__view_path__) ){
 			$__view_path__ = APP_PATH.'views/';
 		}else{
 			$__view_path__ = IN_PATH.$__view_path__.'/';
 		}
 		
+		//视图后缀
+		$__view_suffix__ = $this->get_suffix($__name__);
+
 		//分配变量
 		if(!empty($__data__)){
 			$this->assign($__data__);
@@ -79,12 +82,12 @@ class View{
 		ob_end_clean();
 
 		//是否有布局文件
-		if(empty($this->_layout)){
+		if( empty($this->_layout_path) ){
 			$view = $__view__;
 		}else{
 			$__view__ = trim($__view__) . "\r\n";
 			ob_start ();
-			include($__view_path__.$this->_layout.$__view_suffix__);
+			include($__view_path__.$this->_layout_path);
 			$view = ob_get_contents();
 			ob_end_clean ();
 		}
@@ -95,6 +98,16 @@ class View{
 		}else{
 			echo $view;
 		}
+	}
+	
+	//获取视图文件后缀
+	public function get_suffix($name){
+		$view_suffix = '';
+		$file_type = pathinfo($name, PATHINFO_EXTENSION);
+		if( empty($file_type) ){
+			$view_suffix = Yiee::conf('view_suffix') ? Yiee::conf('view_suffix') : '.php';
+		}
+		return $view_suffix;
 	}
 	
 }
