b989bc74227f23be0ec8a9f702950873080a83f1
rushui <rushui@5291143e-60c1-428a-9f47-b12c1fb71de0>
2015-07-09 09:59:38 +0000

    result_one改为row

--- trunk/application/controllers/dirtest/example.php
+++ trunk/application/controllers/dirtest/example.php
@@ -21,8 +21,8 @@
 			echo '<br />';
 			//echo $this->rs();		//MY_Controller扩展控制器中的方法
 			//** //数据库操作测试
-			$re = $this->db->table('main_keylib')->where(array('status'=>'1'))->fields(array('id','title'))->limit(2,3)->get();
-			$row = $re->result_one();
+			$re = $this->db->table('main_keylib')->where(array('status'=>'1'))->fields(array('id','title'))->limit(5)->get();
+			$row = $re->row(2);
 			$sql = $re->num_rows();
 
 //			$arr = array('title'=>'小李323','ckey'=>'test144');
